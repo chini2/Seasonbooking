433d7a01f682971c8b980160e7ccea189158d9dd
chini2 <juanpablo.y1998@gmail.com>
2021-11-27 16:45:34 -0300

    agregadas inventario, reportes

---
+++ inventory/forms.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# Create your models here.
+ 
+ from .models import InventoryItem
+from apps.items.models import Item
+
+class InventoryItemForm(forms.ModelForm):
+    item = forms.ModelChoiceField(queryset=Item.objects.all())
+
+    class Meta:
+        model = InventoryItem
+        fields = ['quantity','item', 'inventory']
+        labels = {
+            "quantity": "Cantidad",
+            "item" : "Item",
+            "inventory":"Inventario",
+            }
+
+        widgets = {
+            'inventory': forms.HiddenInput()
+        }
---
+++ items/forms.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+
+from django import forms
+from .models import Item
+
+class ItemForm(forms.ModelForm):
+    class Meta:
+        model = Item
+        fields = ['name', 'color', 'size', 'description']
+        labels = {
+            "name": "Nombre",
+            "color": "Color",
+            "size": "Tamaño",
+            "description": "Descripcion",
+        }
